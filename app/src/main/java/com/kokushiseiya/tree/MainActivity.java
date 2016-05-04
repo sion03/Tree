9122fe5441eb39de1e9926dbff9a7cd56e778ab9
Seiya Kokushi <xxx.xyz-s-.xxx@i.softbank.jp>
2016-05-04 19:45:17 +0900

    make login and signup

--- app/src/main/java/com/kokushiseiya/tree/MainActivity.java
+++ app/src/main/java/com/kokushiseiya/tree/MainActivity.java
@@ -29,10 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // toolbarの設定
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         // firebaseの設定
         Firebase.setAndroidContext(this);
         mRootRef = new Firebase(BuildConfig.FIREBASE_URL);
@@ -54,27 +50,5 @@ public class MainActivity extends AppCompatActivity {
         }
         fragmentTransaction.add(R.id.fragment_container, horizontalFragment);
         fragmentTransaction.commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
